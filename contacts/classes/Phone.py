a94334fa76f0e4749c0e9624846a15e828db4ea9
SBogdanS <sbodia09@gmail.com>
2023-12-16 00:43:28 +0000

    class phone

--- contacts/classes/Phone.py
+++ contacts/classes/Phone.py
@@ -1,3 +1,10 @@
 from Field import Field
+
+
 class Phone(Field):
-    pass
+    @Field.value.setter
+    def value(self, value):
+        if value.isdigit() and len(value) == 10:
+            self.__value = value
+        else:
+            raise ValueError("Phone number must be a ten digit string of digits")
