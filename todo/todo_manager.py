7d7f1d094324eafa427a2ede8c954717829015a1
Olga Mageria <olgamageria@gmail.com>
2023-12-21 18:56:17 +0200

    todo changes
    Co-Authored-By: Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>

--- todo/todo_manager.py
+++ todo/todo_manager.py
@@ -4,14 +4,13 @@ import json
 from contacts.classes.Field import Field
 from contacts.classes.Birthday import Birthday
 
-
 class ToDoRecord:
-    def __init__(self, task, begin, end, status=None):
+    def __init__(self, task, begin, end, status=None, tags=None):
         self.task = task
         self.begin = begin
         self.end = end
         self.status = status
-        self.tags = []
+        self.tags = list(tags)
 
     # {'task def add_task(self, task, date, status, tags):
     #     #     self.tasks.append(': task, 'date': date, 'status': status, 'tags': tags})
@@ -121,8 +120,8 @@ class ToDoRecord:
     def edit_status(self, new_status):
         self.status = new_status
 
-    def __str__(self):
-        return f"{self.task}: start - {self.begin}, end - {self.end}, current status is {self.status}, tags: {', '.join(p.value for p in self.tags)}"
+    # def __str__(self):
+    #     return f"{self.task}: start - {self.begin}, end - {self.end}, current status is {self.status}, tags: {', '.join(p.value for p in self.tags)}"
 
 
 class ToDoBook(UserDict):
@@ -132,18 +131,50 @@ class ToDoBook(UserDict):
 
     # I think "self.data[record.task] = record" will be correct
 
-    def search_task(self):
+    def edit_todo(self):
         search_info = input("Enter info for search: ").lower()
+        result_list = []
         for task in self.data.values():
             search_by_task = task.task.lower().find(search_info)
             search_by_date1 = str(task.begin).find(search_info)
             search_by_date2 = str(task.end).find(search_info)
             search_by_status = task.status.lower().find(search_info)
-            search_by_tags = str([tag.value.lower() for tag in task.tags]).find(search_info)
+            search_by_tags = str([tag.lower() for tag in task.tags]).find(search_info)
             if search_by_task > -1 or search_by_date1 > -1 or search_by_date2 > -1 or search_by_status > -1 or search_by_tags > -1:
-                print(task)
+                result_list.append(task)
+        dict_with_number = dict(zip([i + 1 for i in range(len(result_list))], [i.task for i in result_list]))
+        print(dict_with_number)
 
-    # we can change return info
+        try:
+            choice = int(input('Please choose task number to edit: '))
+            for key, value in dict_with_number.items():
+                if key == choice:
+                    task_for_edit = self.data[value]
+                    part_of_task_to_edit = input("Please choose field to edit (title/begin/end/status/tags): ")
+                    if part_of_task_to_edit == 'title':
+                        new_title = input("Enter new task title: ")
+                        self.data[new_title] = self.data[task_for_edit.task]
+                        del self.data[task_for_edit.task]
+                        task_for_edit.task = new_title
+                    elif part_of_task_to_edit == 'begin':
+                        new_begin = input("Enter new begin date: ")
+                        task_for_edit.begin = new_begin
+                    elif part_of_task_to_edit == 'end':
+                        new_end = input("Enter new end date: ")
+                        task_for_edit.end = new_end
+                    elif part_of_task_to_edit == 'status':
+                        new_status = input("Enter new status (Done/In_progress): ")
+                        task_for_edit.status = new_status
+                    elif part_of_task_to_edit == 'tags':
+                        old_tag = input("Enter tag to edit: ")
+                        new_tag = input("Enter new tag: ")
+                        for tag in task_for_edit.tags:
+                            if tag == old_tag:
+                                task_for_edit.tags.remove(old_tag)
+                                task_for_edit.tags.append(new_tag)
+            self.save_to_file_todo('outputs/todo.json')
+        except ValueError:
+                print("Invalid input. Please enter a valid number.")
 
     def delete_task(self):
         delete_info = input("Enter info for delete: ").lower()
@@ -163,39 +194,80 @@ class ToDoBook(UserDict):
             if key == delete_i:
                 del self.data[value]
 
+    def convert_to_serializable_todo(todobook):
+        serializable_data = {}
+        for key, record in todobook.items():
+            serializable_data[str(key)] = {
+                "task": record.task,
+                "begin date": record.begin,
+                "end date": record.end,
+                "status": record.status,
+                "tags": record.tags
+            }
+        return serializable_data
+    def save_to_file_todo(self, filename):
+        data_to_serialize = ToDoBook.convert_to_serializable_todo(self)
+        try:
+            with open(filename, "w", encoding="utf-8") as json_file:
+                json.dump(data_to_serialize, json_file, indent=4)
+                json_file.write('\n')
+            print(f'Tasks saved to {filename} successfully.')
+        except Exception as e:
+            print(f'Error saving tasks to {filename}: {e}')
 
-class ToDoListSave(ToDoRecord):
-    def save_list(self):
-        with open(self.file, "w", encoding="utf-8") as f:
-            json.dump((self.date, self.task), f)
+
+    def load_from_file(filename):
+        try:
+            with open(filename, "r", encoding="utf-8") as json_file:
+                data = json.load(json_file)
+            todobook = ToDoBook()
+            for key, value in data.items():
+                task = key
+                begin = value["begin date"]
+                end = value["end date"]
+                status = value["status"]
+                tags = value["tags"]
+                record =  ToDoRecord(task, begin, end, status, tags)
+                todobook.add_to_do_record(record)
+            # print(f'Notes loaded from {filename} successfully.')
+            return todobook
+        except Exception as e:
+            print(f'Error loading tags from {filename}: {e}')
+            return ToDoBook()  # Return a new instance in case of an error
+
+
+# class ToDoListSave(ToDoRecord):
+#     def save_list(self):
+#         with open(self.file, "w", encoding="utf-8") as f:
+#             json.dump((self.date, self.task), f)
 
 
 if __name__ == '__main__':
     to_do_list = ToDoBook()
 
-    task1 = ToDoRecord("Buy some food", "13.12.2023", "17.12.2023", "Done")
+    task1 = ToDoRecord("Buy some food", "13.12.2023", "17.12.2023", "Done", 'food')
     task1.add_tags("food")
     task1.add_tags("store")
     to_do_list.add_to_do_record(task1)
 
-    task2 = ToDoRecord("Buy shampoo", "16.12.2023", "20.12.2023", "In progress")
+    task2 = ToDoRecord("Buy shampoo", "16.12.2023", "20.12.2023", "In progress", "beauty")
     task2.add_tags("beauty")
     task2.add_tags("store")
     to_do_list.add_to_do_record(task2)
 
-    task3 = ToDoRecord("Get my nails done", "17.12.2023", "28.12.2023", "In progress")
+    task3 = ToDoRecord("Get my nails done", "17.12.2023", "28.12.2023", "In progress", "beauty")
     task3.add_tags("beauty")
     to_do_list.add_to_do_record(task3)
 
-    task3.edit_tags('beauty', 'salon')
-    task2.delete_tags('beauty')
-    task2.edit_task("shampoo", "soap")
-    task2.edit_begin_date("2023-12-17")
-    task2.edit_end_date("2023-12-21")
-    task2.edit_status("Done")
+    # task3.edit_tags('beauty', 'salon')
+    # task2.delete_tags('beauty')
+    # task2.edit_task("shampoo", "soap")
+    # task2.edit_begin_date("2023-12-17")
+    # task2.edit_end_date("2023-12-21")
+    # task2.edit_status("Done")
 
-    to_do_list.search_task()
-    to_do_list.delete_task()
+    # to_do_list.search_task()
+    # to_do_list.delete_task()
 
     for name, record in to_do_list.data.items():
         print(record)
